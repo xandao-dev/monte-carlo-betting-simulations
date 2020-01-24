59069e1ef021b99c8a87fbe3aaf9f9ecd2f18ec1
xandao6 <alexandrecalilmf@gmail.com>
2020-01-24 10:56:42 -0300

    percentage system improved

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ __author__ = 'Alexandre Calil Martins Fonseca, Github: xandao6'
 
 
 from strategies.fixed_system import fixed_system
+from strategies.percentage_system import percentage_system
 import random
 import matplotlib.pyplot as plt
 
@@ -11,20 +12,32 @@ import matplotlib.pyplot as plt
 # FIXME Odds = 1/(win_rate/100.0) -> where I put the payout?
 
 # General Input
-samples = 100
-win_rate = 0.5850 # win_rate: 0.0000-1.0000
-payout_rate = 0.7800 # payout_rate: 0.0000-2.0000 generally, but you choose
-bankroll = 1000
+samples = 500
+win_rate = 0.5000 # win_rate: 0.0000-1.0000
+payout_rate = 1.0000 # payout_rate: 0.0000-2.0000 generally, but you choose
+bankroll = 100
 bet_count = 10000
 
-# Fixed System Input
-bet_percentage = 0.0200 # bet percentage: 0.0000-1.0000
+# Fixed System and Percentage System Input
+bet_percentage = 0.0100 # bet percentage: 0.0000-1.0000
+minimum_bet_value = 2
+stoploss = None
+stopgain = None
 
 
-def main():
-    bust_count = 0
+if bankroll*bet_percentage <= minimum_bet_value:
+    bet_percentage = minimum_bet_value/100.0  
+    print(f'Bet size is less than minimum bet value! Adjusting the bet ' +
+          f'percentage to {bet_percentage}')
+    
+    
+def main():  
+
+    bust_count1 = 0
+    bust_count2 = 0 
+    fig, axs = plt.subplots(2)
     for _ in range(samples):        
-        btX, brY, bust = fixed_system(
+        '''btX, brY, bust1 = fixed_system(
             generate_random_bet_result, 
             win_rate, 
             payout_rate,
@@ -32,19 +45,39 @@ def main():
             bet_count,
             bet_percentage
         )
-        
-        if bust:
-            bust_count += 1
-        
-        #plt.subplot(1, 1)
-        plt.plot(btX, brY, linewidth = 0.3)
-        plt.ylabel('Bankroll')
-        plt.xlabel('Bet Count')
-        plt.axhline(bankroll, color = 'b', linewidth = 0.1)
-        plt.axhline(0, color = 'r', linewidth = 2)
-    plt.show()
-    print(f'{bust_count} broke of {samples} samples.')
+        axs[0].plot(btX, brY, linewidth = 0.3)
+        if bust1:
+            bust_count1 += 1'''
+            
+        btX, brY, bankroll2, bust2,stop_gain_reached = percentage_system(
+            generate_random_bet_result, 
+            win_rate, 
+            payout_rate,
+            bankroll, 
+            bet_count,
+            bet_percentage,
+            minimum_bet_value,
+            stoploss,
+            stopgain
+        )
 
+        axs[1].plot(btX, brY, linewidth = 0.8)
+        if bust2:
+            bust_count2 += 1
+
+    axs[0].set_title('Fixed System')
+    axs[1].set_title('Percentage System')
+    # Hide x labels and tick labels for top plots and y ticks for right plots.
+    for ax in axs.flat:
+        ax.label_outer()
+    for ax in axs:
+        ax.set_ylabel('Bankroll')
+        ax.set_xlabel('Bet Count')
+        ax.axhline(bankroll, color = 'b', linewidth = 0.5)
+        ax.axhline(0, color = 'r', linewidth = 2)
+    plt.show()
+    print(f'{bust_count1} broke of {samples} samples.')
+    print(f'{bust_count2} broke of {samples} samples.')
 
 def generate_random_bet_result(win_rate: float) -> bool:
     result = round(random.uniform(0,1),4)
---
+++ strategies/percentage_system.py
@@ -0,0 +1,88 @@
+__author__ = 'Alexandre Calil Martins Fonseca, Github: xandao6'
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from typing import Callable, Union, Tuple
+
+
+def percentage_system(
+        gen_bet_result: Callable[[float], bool], 
+        win_rate: float,
+        payout_rate: float,
+        bankroll: Union[int, float], 
+        bet_count: int,
+        bet_percentage: float,
+        minimum_bet_value: int,
+        stoploss: Union[int, None],
+        stopgain: Union[int, None]
+) -> Tuple[int, Union[int, float]]:
+    '''
+    Parameters
+    ----------
+    gen_bet_result -> Callable[[float], bool]
+        A function that generate a random bet result, considering the win rate,
+        and return True for win or False for lose.
+    win_rate -> float
+        The win rate is a rate that can range from 0.0000 to 1.0000, which 
+        means the percentage you have of winning. 
+        To know your win rate you must divide the total bets you won by the 
+        total bet, the more bets the more 
+        accurate that rate will be.
+    payout_rate -> float
+        The payout rate means how much you will win, for example a 0.80 payout
+        rate means that for every $ 1 wagered you will win $1 * 0.80 = 0.80c. 
+        Here the payout rate varies from less infinite to more infinite, but 
+        this value generally ranges from 0.0000 to 2.0000.
+    bankroll -> Union[int, float]
+        The bankroll is the amount of money you have to bet.
+    bet_count -> int
+        The bet count is the amount of bets you will simulate.
+    bet_percentage -> float
+        The bet percentage is the amount you will risk on each bet. This value
+        can range from 0.0000 to 1.0000.
+
+    Returns
+    -------
+    Tuple[int, Union[int, float]]
+        This function returns a tuple containing two lists and a int. The 
+        first is the X axis which is the amount of bets. The second is the 
+        Y axis which is the bankroll history. The third is whether it is bust 
+        or not.
+    '''
+    bust = False
+    stop_gain_reached = False
+    
+    bet_count_history_X = []
+    bankroll_history_Y = []
+    for current_bet in range(1, bet_count+1):
+        bet_size = bankroll*bet_percentage
+        
+        if bet_size < minimum_bet_value:
+            bust = True
+            break
+
+        # bankroll <= 1 because it will never bust if we dont use this.
+        if bankroll <= 1:
+            bust = True
+            break
+        
+        if stoploss is not None:
+            if bankroll <= stoploss:
+                bust = True
+                break
+        
+        if stopgain is not None:
+            if bankroll >= stopgain:
+                stop_gain_reached = True
+                break
+            
+        if gen_bet_result(win_rate):
+            bankroll += bet_size*payout_rate
+        else:
+            bankroll -= bet_size
+            
+        bet_count_history_X.append(current_bet)
+        bankroll_history_Y.append(bankroll)
+    return bet_count_history_X, bankroll_history_Y, \
+           bankroll, bust, stop_gain_reached
