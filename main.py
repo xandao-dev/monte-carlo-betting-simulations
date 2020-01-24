3713ae2f5a5f3798f102f3428a448ea3a88cff89
xandao6 <alexandrecalilmf@gmail.com>
2020-01-24 01:19:20 -0300

    added payout and description to fixed system

--- main.py
+++ main.py
@@ -7,14 +7,37 @@ from strategies.fixed_system import fixed_system
 import random
 import matplotlib.pyplot as plt
 
+# Notes
+# FIXME Odds = 1/(win_rate/100.0) -> where I put the payout?
+
 # General Input
 samples = 20
-win_rate = 50 # win_rate 0-100
+win_rate = 55 # win_rate: 0-100
+payout_rate = 80 # payout_rate: 0-100 generally, but you choose
 bankroll = 10000
-wager_count = 1000
+bet_count = 1000
 
 # Fixed System Input
 bet_percentage = 2 # bet percentage 0-100
+
+
+def main():
+    for _ in range(samples):        
+        btX, brY = fixed_system(
+            generate_random_bet_result, 
+            win_rate, 
+            payout_rate,
+            bankroll, 
+            bet_count,
+            bet_percentage
+        )
+        #plt.subplot(1, 1)
+        plt.plot(btX, brY, linewidth = 0.6)
+        plt.ylabel('Bankroll')
+        plt.xlabel('Bet Count')
+        plt.axhline(bankroll, color = 'b', linewidth = 0.1)
+        plt.axhline(0, color = 'r', linewidth = 2)
+    plt.show()
 
 
 def generate_random_bet_result(win_rate: int) -> bool:
@@ -24,20 +47,6 @@ def generate_random_bet_result(win_rate: int) -> bool:
     elif result > win_rate: # (51-100 -> 50%) ----- (61-100 -> 40%)
         return False
     
-
-for _ in range(samples):        
-    wX, bY = fixed_system(
-        generate_random_bet_result, 
-        win_rate, bankroll, 
-        wager_count,
-        bet_percentage
-    )
-    #plt.subplot(1, 1)
-    plt.plot(wX, bY)
-    plt.ylabel('Bankroll')
-    plt.xlabel('Wager Count')
-    plt.axhline(10000, color = 'b')
-    plt.axhline(0, color = 'r')
-    x+=1
-
-plt.show()
+    
+if __name__ == '__main__':
+    main()
--- strategies/fixed_system.py
+++ strategies/fixed_system.py
@@ -8,27 +8,60 @@ from typing import Callable, Union, Tuple
 
 def fixed_system(
         gen_bet_result: Callable[[int], bool], 
-        win_rate: int,  
+        win_rate: int,
+        payout_rate: int,
         bankroll: Union[int, float], 
-        wager_count: int,
+        bet_count: int,
         bet_percentage: int
 ) -> Tuple[int, Union[int, float]]:
+    '''
+    Parameters
+    ----------
+    gen_bet_result -> Callable[[int], bool]
+        A function that generate a random bet result, considering the win rate,
+        and return True for win or False for lose.
+    win_rate -> int
+        The win rate is a rate that can range from 0 to 100, which means the 
+        percentage you have of winning. 
+        To know your win rate you must divide the total bets you won by the 
+        total bet, the more bets the more 
+        accurate that rate will be.
+    payout_rate -> int
+        The payout rate means how much you will win, for example an 80% payout
+        rate means that for every $ 1 wagered you will win $1 * 80% = 0.80c. 
+        Here the payout rate varies from less infinite to more infinite, but 
+        this value generally ranges from 0 to 200.
+    bankroll -> Union[int, float]
+        The bankroll is the amount of money you have to bet.
+    bet_count -> int
+        The bet count is the amount of bets you will simulate.
+    bet_percentage -> int
+        The bet percentage is the amount you will risk on each bet.
+
+    Returns
+    -------
+    Tuple[int, Union[int, float]]
+        This function returns a tuple containing two lists, the first is the 
+        X axis which is the amount of bets, the second is the Y axis which is 
+        the bankroll history.
+
+    '''
     
-    wager_count_history_X = []
+    bet_count_history_X = []
     bankroll_history_Y = []
-    wager = bankroll*bet_percentage/100.0
+    bet_size = bankroll*bet_percentage/100.0
     currentWager = 1
     
     while currentWager <= wager_count:
         if gen_bet_result(win_rate):
-            bankroll += wager
+            bankroll += bet_size*payout/100.0
         else:
-            bankroll -= wager
+            bankroll -= bet_size
             if bankroll <= 0:
                 print('Broke')
                 break
-        wager_count_history_X.append(currentWager)
+        bet_count_history_X.append(currentWager)
         bankroll_history_Y.append(bankroll)
         currentWager += 1
         
-    return wager_count_history_X, bankroll_history_Y
+    return bet_count_history_X, bankroll_history_Y
