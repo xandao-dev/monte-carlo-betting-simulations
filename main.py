8a875aa4e46c26144e1877e9fa4bfba38ac9baef
xandao6 <alexandrecalilmf@gmail.com>
2020-01-28 02:00:35 -0300

    cleaned user interface

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ https://github.com/xandao6/monte-carlo-betting-simulations
 
 Credits: Oliver Wilkins
 https://github.com/HomelessSandwich/MonteCarloBettingSim
-'''
-from betting.strategies import fixed_bettor, percentage_bettor, kelly_criterion
-from betting.Bettor import generate_random_bet_results
+''' 
+from betting.strategies import fixed_bettor#, percentage_bettor, kelly_criterion
+from betting.Bettor import Bettor
 from betting.PlotGraph import PlotGraph
 from typing import Union, List
 
@@ -15,7 +15,7 @@ from typing import Union, List
 user_input = {
     'samples': 10,
     'bet_count': 10000,
-    'win_rate': 0.5600,  # range: 0.0000-1.0000
+    'win_rate': 0.5300,  # range: 0.0000-1.0000
     'payout_rate': 0.8700,  # range: 0.0000-2.0000 generally, but you choose
     'initial_bankroll': 1000,
     'currency': '$',
@@ -32,9 +32,6 @@ user_input = {
 
 
 def main():
-    bet_results = generate_random_bet_results(user_input)
-    plt = PlotGraph(user_input['initial_bankroll'])
-
     x, y = fixed_bettor(bet_results, user_input)
     plt.config('Fixed Bettor', x, y, new_fig=True, color='r')
 
@@ -44,8 +41,9 @@ def main():
     x, y = kelly_criterion(bet_results, user_input)
     plt.config('Kelly Criterion', x, y, new_fig=True, color='b')'''
 
-    plt.show()
-
 
 if __name__ == '__main__':
+    bet_results = Bettor.generate_random_bet_results(user_input)
+    plt = PlotGraph(user_input['initial_bankroll'])
     main()
+    plt.show()
