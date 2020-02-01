2b63aad9e2c32f9275db91b778a431414fba410f
xandao6 <alexandrecalilmf@gmail.com>
2020-02-01 16:02:49 -0300

    added bankroll average to ui

--- betting/ui.py
+++ betting/ui.py
@@ -5,11 +5,22 @@ https://github.com/HomelessSandwich/MonteCarloBettingSim
 HomelessSandwich/MonteCarloBettingSim is licensed under MIT
 '''
 
-def print_stats(samples, bust_count, num_profitors, profits, loses, title):
-    broke_percent = (bust_count / samples) * 100
-    profit_percent = (num_profitors / samples) * 100
+def print_stats(
+    user_input, bankroll_histories, broke_count,
+    profitors_count, profits, loses, title
+) -> None:
+
+    broke_percent = (broke_count / user_input['samples']) * 100
+    profit_percent = (profitors_count / user_input['samples']) * 100
+
+    final_bankroll_sum = 0
+    for bankroll_history in bankroll_histories:
+        final_bankroll_sum += bankroll_history[-1]
+    final_bankroll_average = final_bankroll_sum/user_input['samples']
+
     try:
-        survive_profit_percent = (num_profitors / (samples - bust_count)) * 100
+        survive_profit_percent = (
+            profitors_count / (user_input['samples'] - broke_count)) * 100
     except ZeroDivisionError:
         survive_profit_percent = 0
     try:
@@ -21,11 +32,22 @@ def print_stats(samples, bust_count, num_profitors, profits, loses, title):
     except ZeroDivisionError:
         avg_loses = 0
 
-    print(f'\n*{title.upper()}*\n')
+    print(f'\n*{title.upper()}*')
     print(f'Percentage Broke: {broke_percent}%')
     print(f'Percentage Profited: {profit_percent}%')
     print(f'Percentage Survivors Profited: {survive_profit_percent}%')
+    print(f'Final Bankroll Average: {final_bankroll_average}')
     print(f'Average Profit: {avg_profit}')
     print(f'Average Loses: {avg_loses}')
     print(f'Expected Profit: {avg_profit * (profit_percent/ 100)}')
     print(f'Expected Loss: {avg_loses * (1 - (profit_percent / 100))}')
+
+
+'''
+    print(f'Final bankroll average: {round(bankroll_average,2)}')
+    print(f'Death rate: {round((bust_count/samples)*100,2)}%, '
+          f'Survival rate: {100.0 - round((bust_count/samples)*100,2)}%')
+    print(f'{bust_count} broken of {samples} samples in Fixed Sys.!')
+    print(f'{sl_reached_count} stoploss reached of {samples} in Fixed Sys.!')
+    print(f'{sg_reached_count} stopgain reached of {samples} in Fixed Sys.!\n')
+'''
