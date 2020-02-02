7a4c3f4ebb7aa6b80e805d37630624332253ab73
xandao6 <alexandrecalilmf@gmail.com>
2020-02-02 19:44:37 -0300

    applied strategies changes in main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ https://github.com/xandao6/monte-carlo-betting-simulations
 Credits: Oliver Wilkins
 https://github.com/HomelessSandwich/MonteCarloBettingSim
 ''' 
-from betting.strategies import fixed_bettor#, percentage_bettor, kelly_criterion
+from betting.strategies import fixed_bettor, percentage_bettor, kelly_criterion
 from betting.Bettor import Bettor
 from betting.PlotGraph import PlotGraph
 from typing import Union, List
@@ -13,10 +13,10 @@ from typing import Union, List
 
 # region USER INPUT
 user_input = {
-    'samples': 10,
-    'bet_count': 10000,
-    'win_rate': 0.5300,  # range: 0.0000-1.0000
-    'payout_rate': 0.8700,  # range: 0.0000-2.0000 generally, but you choose
+    'samples': 5,
+    'bet_count': 250,
+    'win_rate': 0.5000,  # range: 0.0000-1.0000
+    'payout_rate': 1.0000,  # range: 0.0000-2.0000 generally, but you choose
     'initial_bankroll': 1000,
     'currency': '$',
     'minimum_bet_value': None,
@@ -24,22 +24,23 @@ user_input = {
     'stoploss': None,
     'stopgain': None,
     #SPECIFIC INPUT
-    'bet_value': 10,  # Fixed System
-    'bet_percentage': 0.0100,  # Percentage System, range: 0.0000-1.0000
+    'bet_value': 100,  # Fixed System
+    'bet_percentage': 0.0800,  # Percentage System, range: 0.0000-1.0000
     'kelly_fraction': 1  # Kelly Criterion, range: 0.0000 to +inf, gen. 1 or 0.5
 }
 # endregion
 
 
 def main():
-    x, y = fixed_bettor(bet_results, user_input)
-    plt.config('Fixed Bettor', x, y, new_fig=True, color='r')
+    x, y, title = fixed_bettor(bet_results, user_input)
+    #FIXME color with new_fig does nothing
+    plt.config(title, x, y, new_fig=True, color='r')
 
-    '''x, y = percentage_bettor(bet_results, user_input)
-    plt.config('Percentage Bettor', x, y, new_fig=True, color='g')
+    x, y, title = percentage_bettor(bet_results, user_input)
+    plt.config(title, x, y, new_fig=True, color='g')
 
-    x, y = kelly_criterion(bet_results, user_input)
-    plt.config('Kelly Criterion', x, y, new_fig=True, color='b')'''
+    x, y, title = kelly_criterion(bet_results, user_input)
+    plt.config(title, x, y, new_fig=True, color='b')
 
 
 if __name__ == '__main__':
