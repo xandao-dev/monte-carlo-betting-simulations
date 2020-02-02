c2ab37d915bbd515802793babd84f25fe8a9cd18
xandao6 <alexandrecalilmf@gmail.com>
2020-02-02 19:43:43 -0300

    values are rounded now

--- betting/ui.py
+++ betting/ui.py
@@ -11,25 +11,25 @@ def print_stats(
     profits, loses, title, kelly_percentage=None
 ) -> None:
 
-    broke_percent = (broke_count / user_input['samples']) * 100
-    profit_percent = (profitors_count / user_input['samples']) * 100
+    broke_percent = round((broke_count / user_input['samples']) * 100, 2)
+    profit_percent = round((profitors_count / user_input['samples']) * 100, 2)
 
     final_bankroll_sum = 0
     for bankroll_history in bankroll_histories:
         final_bankroll_sum += bankroll_history[-1]
-    final_bankroll_average = final_bankroll_sum/user_input['samples']
+    final_bankroll_average = round(final_bankroll_sum/user_input['samples'], 2)
 
     try:
-        survive_profit_percent = (
-            profitors_count / (user_input['samples'] - broke_count)) * 100
+        survive_profit_percent = round(
+            (profitors_count / (user_input['samples'] - broke_count)) * 100, 2)
     except ZeroDivisionError:
         survive_profit_percent = 0
     try:
-        avg_profit = sum(profits) / len(profits)
+        avg_profit = round(sum(profits) / len(profits), 2)
     except ZeroDivisionError:
         avg_profit = 0
     try:
-        avg_loses = sum(loses) / len(loses)
+        avg_loses = round(sum(loses) / len(loses), 2)
     except ZeroDivisionError:
         avg_loses = 0
 
@@ -43,8 +43,8 @@ def print_stats(
     print(f'Final Bankroll Average: {final_bankroll_average}')
     print(f'Average Profit: {avg_profit}')
     print(f'Average Loses: {avg_loses}')
-    print(f'Expected Profit: {avg_profit * (profit_percent/ 100)}')
-    print(f'Expected Loss: {avg_loses * (1 - (profit_percent / 100))}')
+    print(f'Expected Profit: {round(avg_profit * (profit_percent/ 100), 2)}')
+    print(f'Expected Loss: {round(avg_loses * (1 - (profit_percent / 100)), 2)}')
 
 
 '''
