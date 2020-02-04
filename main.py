146c3ed6af54e88e8b6cc0abf5c0faee42627f1e
xandao6 <alexandrecalilmf@gmail.com>
2020-02-03 22:55:52 -0300

    strategies activated

--- main.py
+++ main.py
@@ -14,33 +14,33 @@ from typing import Union, List
 
 # region USER INPUT
 user_input = {
-    'samples': 1,
-    'bet_count': 2,
-    'win_rate': 0.5000,  # range: 0.0000-1.0000
-    'payout_rate': 1.0000,  # range: 0.0000-2.0000 generally, but you choose
+    'samples': 250,
+    'bet_count': 5000,
+    'win_rate': 0.5500,  # range: 0.0000-1.0000
+    'payout_rate': 0.8500,  # range: 0.0000-2.0000 generally, but you choose
     'initial_bankroll': 1000,
     'currency': '$',
-    'minimum_bet_value': None,
+    'minimum_bet_value': 2,
     'maximum_bet_value': None,
     'stoploss': None,
     'stopgain': None,
     # SPECIFIC INPUT
     'bet_value': 10,  # Fixed System
-    'bet_percentage': 0.0800,  # Percentage System, range: 0.0000-1.0000
+    'bet_percentage': 0.0100,  # Percentage System, range: 0.0000-1.0000
     'kelly_fraction': 1  # Kelly Criterion, range: 0.0000 to +inf, gen. 1 or 0.5
 }
 # endregion
 
 
 def main():
-    #x, y, title = fixed_bettor(bet_results, user_input)
-    #plt.config(title, x, y, new_fig=True, color='r')
+    x, y, title = fixed_bettor(bet_results, user_input)
+    plt.config(title, x, y, new_fig=True, color='r')
 
-    #x, y, title = percentage_bettor(bet_results, user_input)
-    #plt.config(title, x, y, new_fig=True, color='g')
+    x, y, title = percentage_bettor(bet_results, user_input)
+    plt.config(title, x, y, new_fig=True, color='g')
 
-    #x, y, title = kelly_criterion(bet_results, user_input)
-    #plt.config(title, x, y, new_fig=True, color='b')
+    x, y, title = kelly_criterion(bet_results, user_input)
+    plt.config(title, x, y, new_fig=True, color='b')
 
     x, y, title = martingale(bet_results, user_input)
     plt.config(title, x, y, new_fig=True, color='c')
