8ace6b521456ab5a0ac97a961fb4fbddfc5d8a2c
xandao6 <alexandrecalilmf@gmail.com>
2020-02-05 04:25:52 -0300

    added percentage_martingale

--- betting/strategies.py
+++ betting/strategies.py
@@ -198,7 +198,7 @@ def kelly_criterion(
 def fixed_martingale(
     bet_results,
     user_input,
-    title='Martingale',
+    title='Fixed Martingale',
     bet_value=None,
     multiplication_factor=2,
     round_limit=10,
@@ -227,6 +227,87 @@ def fixed_martingale(
         current_bankroll = user_input['initial_bankroll']
 
         for i, bet_result in enumerate(sample_result):
+            if not inverted:
+                if sample_result[i-1] == False and i > 0 and current_round < round_limit:
+                    bet_value = multiplication_factor*bet_value
+                    bet_value = bettor.max_min_verify(bet_value)
+                    current_round += 1
+                else:
+                    bet_value = initial_bet_value
+                    current_round = 0
+            else:
+                if sample_result[i-1] == True and i > 0 and current_round < round_limit:
+                    bet_value = multiplication_factor*bet_value
+                    bet_value = bettor.max_min_verify(bet_value)
+                    current_round += 1
+                else:
+                    bet_value = initial_bet_value
+                    current_round = 0
+
+            current_bankroll = bettor.bet(
+                bet_result, bet_value, current_bankroll)
+
+            broke = bettor.broke_verify(current_bankroll)
+            stoploss_reached = bettor.stoploss_verify(current_bankroll)
+            stopgain_reached = bettor.stopgain_verify(current_bankroll)
+            if broke or stoploss_reached or stopgain_reached:
+                current_bankroll = bankroll_history[-1]
+                break
+            bankroll_history.append(current_bankroll)
+
+        if bettor.profit(current_bankroll) > 0 and not (broke or stoploss_reached):
+            profitors_count += 1
+            profits.append(bettor.profit(current_bankroll))
+        else:
+            loses.append(bettor.profit(current_bankroll))
+        if broke:
+            broke_count += 1
+        if stoploss_reached:
+            sl_reached_count += 1
+        if stopgain_reached:
+            sg_reached_count += 1
+
+        bankroll_histories.append(bankroll_history.copy())
+    bet_count_histories = bettor.get_bet_count_histories(bankroll_histories)
+
+    print_stats(
+        user_input, bankroll_histories, broke_count,
+        profitors_count, profits, loses, title)
+    return bet_count_histories, bankroll_histories, title
+
+
+def percentage_martingale(
+    bet_results,
+    user_input,
+    title='Percentage Martingale',
+    bet_percentage=None,
+    multiplication_factor=2,
+    round_limit=10,
+    inverted=False
+) -> Tuple[List[List[int]], List[List[Union[int, float]]], str]:
+    bettor = Bettor(user_input)
+
+    current_round = 0
+    sl_reached_count = 0
+    sg_reached_count = 0
+    broke_count = 0
+    profitors_count = 0
+
+    profits = []
+    loses = []
+    bet_count_histories = []
+    bankroll_histories = []
+
+    if bet_percentage is None:
+        bet_percentage = user_input['bet_percentage']
+
+    for sample_result in bet_results:
+        bankroll_history = [user_input['initial_bankroll']]
+        current_bankroll = user_input['initial_bankroll']
+
+        for i, bet_result in enumerate(sample_result):
+            initial_bet_value = current_bankroll*bet_percentage
+            initial_bet_value = bettor.max_min_verify(initial_bet_value)
             if not inverted:
                 if sample_result[i-1] == False and i > 0 and current_round < round_limit:
                     bet_value = multiplication_factor*bet_value
