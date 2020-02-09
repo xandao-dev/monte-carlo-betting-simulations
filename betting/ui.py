9dffa686bf3e4eda7d0df080f7f81f6e823bb916
xandao6 <alexandrecalilmf@gmail.com>
2020-02-09 19:39:12 -0300

    created a print tutorial

--- betting/ui.py
+++ betting/ui.py
@@ -5,8 +5,46 @@ https://github.com/HomelessSandwich/MonteCarloBettingSim
 HomelessSandwich/MonteCarloBettingSim is licensed under MIT
 '''
 
+from betting.statistical_calculations import *
 
-def print_stats(
+
+def print_indicators_tutorial(language=None):
+    if language == 'PORTUGUESE':
+        print('''Expected Rate of Return (Taxa de Retorno Esperada):  Um RoR de 
+3% significa que você tende a ganhar 3% do valor da sua aposta no longo prazo.''')
+        print('''Kelly criterion in percentage of capital (Critério de Kelly em 
+porcentagem de capital): ''')
+        #print('''Risk of Ruin(Risco de Ruína): ''')
+        print('''Percentage Broke (Percentual de Quebra): ''')
+        print('''Percentage Profited (Percentual de Lucro): ''')
+        print('''Percentage Survivors Profited (Percentual de Sobreviventes que 
+        Lucraram): ''')
+        print('''Final Bankroll Average (Média Final da Banca): ''')
+        print('''Average Profit (Média dos Lucros): ''')
+        print('''Average Loses (Média das Perdas): ''')
+        print('''Expected Profit (Lucro Esperado): ''')
+        print('''Expected Loss (Perda Esperada): ''')
+    else:
+        print('''Expected Rate of Return:  A RoR of 3% means that you tend to win 
+3% of your stake in the long run.''')
+        print('''Kelly criterion in percentage of capital: ''')
+        #print('''Risk of Ruin: ''')
+        print('''Percentage Broke: ''')
+        print('''Percentage Profited: ''')
+        print('''Percentage Survivors Profited: ''')
+        print('''Final Bankroll Average: ''')
+        print('''Average Profit: ''')
+        print('''Average Loses: ''')
+        print('''Expected Profit: ''')
+        print('''Expected Loss: ''')
+
+
+def print_general_stats(user_input):
+    rate_of_return = calculate_expected_rate_of_return(user_input)
+    print(f'Expected Rate of Return: {rate_of_return}%')
+
+
+def print_strategy_stats(
     user_input, bankroll_histories, broke_count, profitors_count,
     profits, loses, title, kelly_percentage=None
 ) -> None:
@@ -32,23 +70,27 @@ def print_stats(
         avg_loses = round(sum(loses) / len(loses), 2)
     except ZeroDivisionError:
         avg_loses = 0
+
+    risk_of_ruin = calcule_risk_of_ruin(strategies[0], user_input)
+
     print('\n'+'-'*80)
     print(user_input['currency'])
     print(f'*{title.upper()}*')
     if kelly_percentage is not None:
         print('Kelly criterion in percentage of capital: ' +
-              f'{round(kelly_percentage*100,2)}%')
+              f'{round(kelly_percentage*100,2)}%\n')
+    #print(f'Risk of Ruin: {risk_of_ruin}%')
     print(f'Percentage Broke: {broke_percent}%')
     print(f'Percentage Profited: {profit_percent}%')
-    print(f'Percentage Survivors Profited: {survive_profit_percent}%')
+    print(f'Percentage Survivors Profited: {survive_profit_percent}%\n')
     print(
         f'Final Bankroll Average: {user_input["currency"]} {final_bankroll_average}')
     print(f'Average Profit: {user_input["currency"]} {avg_profit}')
-    print(f'Average Loses: {user_input["currency"]} {avg_loses}')
+    print(f'Average Loses: {user_input["currency"]} {avg_loses}\n')
     print(
         f'Expected Profit: {user_input["currency"]} {round(avg_profit * (profit_percent/ 100), 2)}')
     print(
-        f'Expected Loss: {user_input["currency"]} {round(avg_loses * (1 - (profit_percent / 100)), 2)}')
+        f'Expected Loss: {user_input["currency"]} {round(avg_loses * (1 - (profit_percent / 100)), 2)}\n')
     print('-'*80)
 
 
