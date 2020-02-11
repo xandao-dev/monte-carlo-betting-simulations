95cd428215ffd27e0a4f79fe9bdc9c1f74e60eb5
xandao6 <alexandrecalilmf@gmail.com>
2020-02-11 20:32:02 -0300

    ROI fixed

--- betting/statistical_calculations.py
+++ betting/statistical_calculations.py
@@ -4,12 +4,12 @@ from scipy.stats import binom
 
 def calculate_expected_rate_of_return(user_input):
     '''
-    The expected return is the profit or loss an investor anticipates on an 
-    investment that has known or anticipated rates of return (RoR). It is 
-    calculated by multiplying potential outcomes by the chances of them 
-    occurring and then totaling these results. 
-    For example, if an investment has a 55% chance of gaining 87% and a 45% 
-    chance of losing 100%, the expected return is 2.85% 
+    The expected return is the profit or loss an investor anticipates on an
+    investment that has known or anticipated rates of return (RoR). It is
+    calculated by multiplying potential outcomes by the chances of them
+    occurring and then totaling these results.
+    For example, if an investment has a 55% chance of gaining 87% and a 45%
+    chance of losing 100%, the expected return is 2.85%
     (55% x 87% + 45% x -100% = 2.85%).
 
     A 'ror' of 3% means that you tend to win 3% of your stake in the long run.
@@ -33,12 +33,12 @@ def calculate_CDF_average_from_binomial_distribution(user_input, bet_results):
 # FIXME: I think this formula is wrong because of payout.
 def calcule_risk_of_ruin(current_strategy, user_input):
     '''
-    Risk of ruin is a concept in gambling, insurance, and finance relating to 
-    the likelihood of losing all one's investment capital[1] or extinguishing 
-    one's bankroll below the minimum for further play. For instance, if someone 
-    bets all their money on a simple coin toss, the risk of ruin is 50%. In a 
-    multiple-bet scenario, risk of ruin accumulates with the number of bets: 
-    each repeated play increases the risk, and persistent play ultimately 
+    Risk of ruin is a concept in gambling, insurance, and finance relating to
+    the likelihood of losing all one's investment capital[1] or extinguishing
+    one's bankroll below the minimum for further play. For instance, if someone
+    bets all their money on a simple coin toss, the risk of ruin is 50%. In a
+    multiple-bet scenario, risk of ruin accumulates with the number of bets:
+    each repeated play increases the risk, and persistent play ultimately
     yields the stochastic certainty of gambler's ruin.
 
     Formula: Risk of Ruin = ((1 - (W*R - L)) / (1 + (W*R - L)))^U
@@ -46,7 +46,7 @@ def calcule_risk_of_ruin(current_strategy, user_input):
             W = the probability of a desirable outcome, or a win
             L = the probability of an undesirable outcome, or a loss
             R = Payout Rate, 0 <= R <= 1
-            U = the maximum number of risks that can be taken before the 
+            U = the maximum number of risks that can be taken before the
                 individual reaches their threshold for ruin
     '''
 
@@ -84,12 +84,12 @@ def calculate_survived_profited_percentage(
 def calculate_ROI_percentage_average(user_input, bankroll_histories):
     ROI_sum = 0
     for bankroll_history in bankroll_histories:
-        ROI_sum += (bankroll_history[-1]/user_input['initial_bankroll'])*100
+        ROI_sum += ((bankroll_history[-1] - user_input['initial_bankroll']) /
+                    user_input['initial_bankroll'])*100
     ROI_percentage_average = round(ROI_sum/user_input['samples'], 2)
     return ROI_percentage_average
 
 
-# FIXME: Verify
 def calculate_yield_percentage_average(
         user_input, bankroll_histories, bet_value_histories):
     bet_value_sum = 0
