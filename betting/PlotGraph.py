83d932067d91a4328e8e844d93a48c35c595f881
xandao6 <alexandrecalilmf@gmail.com>
2020-02-13 20:44:14 -0300

    new fig removed

--- betting/PlotGraph.py
+++ betting/PlotGraph.py
@@ -13,62 +13,31 @@ class PlotGraph:
             bet_count_histories: List[List[int]],
             bankroll_histories: List[List[Union[int, float]]],
             title: str,
-            plot_bankroll_average: bool = True,
-            new_fig: bool = True,
-            color: Union[str, None] = None
+            plot_bankroll_average: bool = True
     ) -> None:
-        '''
-        Parameters
-        ----------
-        title -> str
-            The title of the graph.
-        bet_count_histories -> List[List[int]]
-            bet_count_histories is a list that contain the X axis lists which is
-            the amount of bets.
-        bankroll_histories -> List[List[Union[int, float]]]
-            bankroll_histories is a list that contain the Y axis lists which is
-            the bankroll history.
-        plot_bankroll_average -> bool, optional
-            Plot the banklroll average with a big width and red color.
-        new_fig -> bool, optional
-            new_fig is to open a new graph window. The default is True.
-        color -> Union[str, None], optional
-            The default is None.
-        Returns
-        -------
-        None
-        '''
-        if new_fig:
-            plt.figure()
-            for x, y in zip(bet_count_histories, bankroll_histories):
-                plt.plot(x, y, linewidth=0.8)
-            plt.title(title)
-        elif not new_fig and color is None:
-            for x, y in zip(bet_count_histories, bankroll_histories):
-                plt.plot(x, y, linewidth=0.8)
-        else:
-            label_assigned = False
-            for x, y in zip(bet_count_histories, bankroll_histories):
-                if not label_assigned:
-                    plt.plot(x, y, linewidth=0.8, label=title, color=color)
-                    label_assigned = True
-                plt.plot(x, y, linewidth=0.8, color=color)
-            leg = plt.legend()
-            for line in leg.get_lines():
-                line.set_linewidth(4.0)
-        #FIXME
+        plt.figure()
+        [plt.plot(x, y, linewidth=0.8)
+         for x, y in zip(bet_count_histories, bankroll_histories)]
+        plt.title(title)
+
         if plot_bankroll_average:
-            bankroll_history_average = self.__get_bankroll_history_average(
-                bankroll_histories)
-            bet_count_history = self.__get_bet_count_history(
-                bankroll_history_average)
-            plt.plot(bet_count_history, bankroll_history_average, linewidth=3, color='r')
+            self.__config_bankroll_average(bankroll_histories)
 
         plt.ylabel('Bankroll')
         plt.xlabel('Bet Count')
         plt.axhline(self.user_input['initial_bankroll'],
                     color='b', linewidth=1.2)
         plt.axhline(0, color='r', linewidth=1.2)
+
+    def __config_bankroll_average(self, bankroll_histories):
+        bankroll_history_average = self.__get_bankroll_history_average(
+            bankroll_histories)
+        bet_count_history = self.__get_bet_count_history(
+            bankroll_history_average)
+        plt.plot(bet_count_history, bankroll_history_average,
+                 linewidth=2.5, color='k', label='Bankroll Average')
+        leg = plt.legend()
+        [line.set_linewidth(4.0) for line in leg.get_lines()]
 
     def __get_bankroll_history_average(self, bankroll_histories):
         bankroll_history_sum = []
@@ -90,7 +59,8 @@ class PlotGraph:
 
     def __get_bet_count_history(self, bankroll_history_average):
         try:
-            bet_count_history = list(zip(*enumerate(bankroll_history_average, 1)))[0]
+            bet_count_history = list(
+                zip(*enumerate(bankroll_history_average, 1)))[0]
         except IndexError:
             return list()
         return bet_count_history
